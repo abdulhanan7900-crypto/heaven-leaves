37f67ed5c03cb523c7d3d1a44251164e510a32c0
abdulhanan8527 <honeykhan8527@gmail.com>
2025-11-12 18:58:23 +0500

    Issue Fixed

--- heavenleaves/settings.py
+++ heavenleaves/settings.py
@@ -60,14 +60,12 @@ WSGI_APPLICATION = 'heavenleaves.wsgi.application'
 
 # Database configuration
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME', 'postgres'),
-        'USER': os.getenv('DB_USER', 'postgres'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT', '5432'),
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        conn_health_checks=True,
+        ssl_require=True
+    )
 }
 
 # Password validation
